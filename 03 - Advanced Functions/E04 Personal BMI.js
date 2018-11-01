06b3b074a9d227c2e3091358b120727616930a9a
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-11-01 19:45:26 +0200

    Update E04 Personal BMI.js

--- 03 - Advanced Functions/E04 Personal BMI.js
+++ 03 - Advanced Functions/E04 Personal BMI.js
@@ -1,32 +1,32 @@
-// function solve4() {
-//     let [name, age, weight, height] = arguments;
-//     //height / 100 = height in meters
-//     let bmi = weight / Math.pow(height / 100, 2);
+function solve4() {
+    let [name, age, weight, height] = arguments;
+    //height / 100 = height in meters
+    let bmi = weight / Math.pow(height / 100, 2);
 
-//     //need to add stats to result
-//     let result = {
-//         name: name,
-//         personalInfo: {
-//             age: age,
-//             weight: weight,
-//             height: height
-//         },
-//         BMI: Math.round(bmi)
-//     }
+    //need to add stats to result
+    let result = {
+        name: name,
+        personalInfo: {
+            age: age,
+            weight: weight,
+            height: height
+        },
+        BMI: Math.round(bmi)
+    }
 
-//     if (bmi < 18.5) {
-//         result.status = 'underweight';
-//     } else if (bmi < 25) {
-//         result.status = 'normal'
-//     } else if (bmi < 30) {
-//         result.status = 'overweight'
-//     } else {
-//         result.status = 'obese'
-//         result.recommendation = 'admission required'
-//     }
+    if (bmi < 18.5) {
+        result.status = 'underweight';
+    } else if (bmi < 25) {
+        result.status = 'normal'
+    } else if (bmi < 30) {
+        result.status = 'overweight'
+    } else {
+        result.status = 'obese'
+        result.recommendation = 'admission required'
+    }
 
-//     return result;
-// }
+    return result;
+}
 
 // console.log(solve4('Peter', 29, 75, 182));
 // console.log(solve4('Honey Boo Boo', 9, 57, 137));
