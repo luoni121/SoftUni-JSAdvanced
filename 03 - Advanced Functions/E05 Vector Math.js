53f2645cba3f8a762107bb2c9ccc028c409f65f7
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-11-01 19:46:44 +0200

    Create E05 Vector Math.js

---
+++ 03 - Advanced Functions/E05 Vector Math.js
@@ -0,0 +1,32 @@
+// let solution = (function () {
+
+//     function add(v1, v2) {
+//         return [v1[0] + v2[0], v1[1] + v2[1]];
+//     }
+//     function multiply(v1, scalar) {
+//         return [v1[0] * scalar, v1[1] * scalar];
+//     }
+//     function length(v1) {
+//         return Math.sqrt(v1[0] * v1[0] + v1[1] * v1[1]);
+//     }
+//     function dot(v1, v2) {
+//         return v1[0] * v2[0] + v1[1] * v2[1];
+//     }
+//     function cross(v1, v2) {
+//         return v1[0] * v2[1] - v1[1] * v2[0];
+//     }
+
+//     return {
+//         add:add,
+//             multiply:multiply,
+//         length:length,
+//             dot:dot,
+//         cross:cross
+//     }
+// })();
+
+// console.log(solution.add([1, 1], [1, 0]));
+// console.log(solution.multiply([3.5, -2], 2));
+// console.log(solution.length([3, -4]));
+// console.log(solution.dot([1, 0], [0, -1]));
+// console.log(solution.cross([3, 7], [1, 0]));
