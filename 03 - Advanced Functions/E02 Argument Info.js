e7ea89c7a6366a614c1ad5464d403494ec37b860
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-11-01 19:43:30 +0200

    Update E02 Argument Info.js

--- 03 - Advanced Functions/E02 Argument Info.js
+++ 03 - Advanced Functions/E02 Argument Info.js
@@ -1,22 +1,22 @@
-// function solve2() {
-//     let map = new Map();
+function solve2() {
+    let map = new Map();
 
-//     for (const argument of arguments) {
-//         let type = typeof argument;
+    for (const argument of arguments) {
+        let type = typeof argument;
 
-//         if (map.has(type) === false) {
-//             map.set(type, 0);
-//         }
-//         let oldCount = map.get(type);
-//         map.set(type, oldCount + 1);
-//         console.log(`${type}: ${argument}`)
-//     }
+        if (map.has(type) === false) {
+            map.set(type, 0);
+        }
+        let oldCount = map.get(type);
+        map.set(type, oldCount + 1);
+        console.log(`${type}: ${argument}`)
+    }
 
-//     [...map]
-//         .sort((a, b) => b[1] - a[1])
-//         .forEach(e => {
-//             console.log(`${e[0]} = ${e[1]}`)
-//         });
-// }
+    [...map]
+        .sort((a, b) => b[1] - a[1])
+        .forEach(e => {
+            console.log(`${e[0]} = ${e[1]}`)
+        });
+}
 
 // solve2('cat', 42, function () { console.log('Hello world!'); });
