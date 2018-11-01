5a5ae4ed66492377469a7a93f1dec6cbb8c51d0f
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-11-01 19:48:15 +0200

    Update E06 Breakfast Robot.js

--- 03 - Advanced Functions/E06 Breakfast Robot.js
+++ 03 - Advanced Functions/E06 Breakfast Robot.js
@@ -1,65 +1,65 @@
-// function solution(input) {
+function solution(input) {
 
-//     let ingredients = {
-//         protein: 0, carbohydrate: 0, fat: 0, flavour: 0
-//     }
+    let ingredients = {
+        protein: 0, carbohydrate: 0, fat: 0, flavour: 0
+    }
 
-//     let meals = {
-//         apple: { protein: 0, carbohydrate: 1, fat: 0, flavour: 2 },
-//         coke: { protein: 0, carbohydrate: 10, fat: 0, flavour: 20 },
-//         burger: { protein: 0, carbohydrate: 5, fat: 7, flavour: 3 },
-//         omelet: { protein: 5, carbohydrate: 0, fat: 1, flavour: 1 },
-//         cheverme: { protein: 10, carbohydrate: 10, fat: 10, flavour: 10 }
-//     }
+    let meals = {
+        apple: { protein: 0, carbohydrate: 1, fat: 0, flavour: 2 },
+        coke: { protein: 0, carbohydrate: 10, fat: 0, flavour: 20 },
+        burger: { protein: 0, carbohydrate: 5, fat: 7, flavour: 3 },
+        omelet: { protein: 5, carbohydrate: 0, fat: 1, flavour: 1 },
+        cheverme: { protein: 10, carbohydrate: 10, fat: 10, flavour: 10 }
+    }
 
-//     let commands = {
-//         restock: (product, quantity) => {
-//             ingredients[product] += +quantity;
-//             return 'Success';
-//         },
-//         prepare: (meal, quantity) => {
+    let commands = {
+        restock: (product, quantity) => {
+            ingredients[product] += +quantity;
+            return 'Success';
+        },
+        prepare: (meal, quantity) => {
 
-//             if (ingredients.protein < meals[meal].protein * quantity) {
-//                 return 'Error: not enough protein in stock';
-//             }
+            if (ingredients.protein < meals[meal].protein * quantity) {
+                return 'Error: not enough protein in stock';
+            }
 
-//             else if (ingredients.carbohydrate < meals[meal].carbohydrate * quantity) {
-//                 return 'Error: not enough carbohydrate in stock';
-//             }
+            else if (ingredients.carbohydrate < meals[meal].carbohydrate * quantity) {
+                return 'Error: not enough carbohydrate in stock';
+            }
 
-//             else if (ingredients.fat < meals[meal].fat * quantity) {
-//                 return 'Error: not enough fat in stock';
-//             }
+            else if (ingredients.fat < meals[meal].fat * quantity) {
+                return 'Error: not enough fat in stock';
+            }
 
-//             else if (ingredients.flavour < meals[meal].flavour * quantity) {
-//                 return 'Error: not enough flavour in stock';
-//             }
-//             else {
-//                 let needed = meals[meal];
-//                 for (let [key, value] of Object.entries(needed)) {
-//                     ingredients[key] -= value * quantity;
-//                 }
-//                 return 'Success';
-//             }
-//         },
-//         report: () => {
-//             return `protein=${ingredients.protein} carbohydrate=${ingredients.carbohydrate} ` +
-//                 `fat=${ingredients.fat} flavour=${ingredients.flavour}`
-//         }
-//     }
+            else if (ingredients.flavour < meals[meal].flavour * quantity) {
+                return 'Error: not enough flavour in stock';
+            }
+            else {
+                let needed = meals[meal];
+                for (let [key, value] of Object.entries(needed)) {
+                    ingredients[key] -= value * quantity;
+                }
+                return 'Success';
+            }
+        },
+        report: () => {
+            return `protein=${ingredients.protein} carbohydrate=${ingredients.carbohydrate} ` +
+                `fat=${ingredients.fat} flavour=${ingredients.flavour}`
+        }
+    }
 
-//     return function preapre(input) {
-//         input = input.split(' ');
-//         token = input.shift();
-//         switch (token) {
-//             case 'report':
-//                 return commands[token]();
-//             case 'restock':
-//             case 'prepare':
-//                 return commands[token](input.shift(), input.shift());
-//         }
-//     }
-// }
+    return function preapre(input) {
+        input = input.split(' ');
+        token = input.shift();
+        switch (token) {
+            case 'report':
+                return commands[token]();
+            case 'restock':
+            case 'prepare':
+                return commands[token](input.shift(), input.shift());
+        }
+    }
+}
 
 // let manager = solution();
 // console.log(manager("restock flavour 50")); // Success
